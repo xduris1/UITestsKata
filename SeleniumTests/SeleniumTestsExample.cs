ca14be94d71e116d5ac2e34429658ec623ad8e00
miroslavkubus <433645@muni.cz>
2022-04-25 22:28:03 +0200

    feat: tests template

---
+++ SeleniumTests/SeleniumTestsExample.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    public class SeleniumTestsExample
+    {
+        IWebDriver? driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            // TODO: Place here path to you chrome driver
+            driver = new ChromeDriver("");
+        }
+
+        [Test]
+        public void OrderProcessTest()
+        {
+            driver.Url = "https://www.saucedemo.com/inventory.html";
+
+            // TODO: Place your test code here
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+
+}
